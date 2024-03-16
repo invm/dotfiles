334aa52d9618758009b43cca268c21e51bc9edd7
Michael Ionov <imichaelionov@gmail.com>
2024-03-16 09:29:49 +0200

    chore: add zsh completions path

--- scripts/for.zsh
+++ scripts/for.zsh
@@ -1,4 +1,11 @@
 # add to .zshrc
+
+# After plugins
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+
+# load zoxide
+eval "$(zoxide init zsh)"
+
 # Load seperated config files
 for conf in "$HOME/.config/zsh/"*.zsh; do
   source "${conf}"
