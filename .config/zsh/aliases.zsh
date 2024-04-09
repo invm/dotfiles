c8cec5e7de5bcc3106447a801ebcb8e3b8f45b12
Michael Ionov <michael@appdome.com>
2024-04-09 11:22:02 +0300

    chore: add kubectl aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias cl=clear
 alias k=kubectl
 alias f="kubectl logs -f"
 alias d="kubectl describe"
+alias wpod="kubectl get pods -A --no-headers | fzf | awk '{ print \$2 }' | xargs -I {} sh -c 'watch \"kubectl get pods -A | grep {} \"'"
+alias lpod="kubectl get pods --no-headers | fzf | awk '{ print \$1 }' | xargs -I {} sh -c 'kubectl logs -f {}'"
