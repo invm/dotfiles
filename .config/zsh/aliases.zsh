45d34efd70ad2a5bea033d169479f6c4e23275cd
Michael Ionov <michael@appdome.com>
2024-05-27 09:16:35 +0300

    feat(zsh): add fzf history

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -11,3 +11,9 @@ alias f="kubectl logs -f"
 alias d="kubectl describe"
 alias wpod="kubectl get pods -A --no-headers | fzf | awk '{ print \$2 }' | xargs -I {} sh -c 'watch \"kubectl get pods -A | grep {} \"'"
 alias lpod="kubectl get pods --no-headers | fzf | awk '{ print \$1 }' | xargs -I {} sh -c 'kubectl logs -f {}'"
+
+source <(fzf --zsh)
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+setopt appendhistory
